e8b16d1b3b8c3f550d97656ab31aa86a1249928e
Anton <oxlamons@gmail.com>
2020-09-07 16:04:54 +0300

    создание  DigitalOcean

---
+++ variable.tf
@@ -0,0 +1,6 @@
+variable "do_token" {
+  default = "bd59fe55f33547470e86eecbcb07e71ecdc0ab07b06d1302acdf82b952a0d321"
+}
+variable "my_ssh_key" {
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC2g1JJtRylAPIee6PJAPBnaF070h9PS7kjJ5vcU8bb1qQa6u3sgAA46riJlNRRMkKI2fXlqZFquqtDniRTxzCaGzbJw3nve1HB9c6XOyvbrR2yjDjFR02SQncD1v1yPqkLcUh93cQNxAhoDPVbmq1wgqcAzJR2wE1/dC7PDVvkpy+jbcQzMj2gfR6tGDb7YJD3XpgbH6sT80FFDlZYgsGaUmumYA0wHW61aRDyzYpRVayYeGsQbRoEV6kOGikhc4gijS5hfU/Ckbw+O7QqSAON18eTgChTLrpqNxb/FDxuV5rKUOMPgWjMrzyqwX3/0XBEhjNDX9zvk/3XuqTfn6qN"
+}
